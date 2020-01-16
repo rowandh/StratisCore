5133e54e997b4aa8561f350e18589287a43f3f12
Rowan de Haas <rowandehaas@outlook.com>
2020-01-16 15:09:06 +1100

    Add decimals to token

--- StratisCore.UI/src/app/wallet/tokens/models/token.ts
+++ StratisCore.UI/src/app/wallet/tokens/models/token.ts
@@ -1,18 +1,20 @@
 export class Token {
-  constructor(ticker: string, address: string, name: string) {
+  constructor(ticker: string, address: string, name: string, decimals: number = 0) {
     this.ticker = ticker;
     this.address = address;
     this.name = name || this.ticker;
+    this.decimals = decimals;
   }
 
   ticker: string;
   address: string;
   name: string;
+  decimals: number;
 }
 
 export class SavedToken extends Token {
-  constructor(ticker: string, address: string, balance: number, name: string) {
-    super(ticker, address, name);
+  constructor(ticker: string, address: string, balance: number, name: string, decimals: number = 0) {
+    super(ticker, address, name, decimals);
     this.balance = balance;
   }
 
